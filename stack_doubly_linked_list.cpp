e4b8d63a81c1c3307c7b23e241111035bcdb12a4
memonul-islam <memonulislam@gmail.com>
2025-07-20 14:03:46 +0600

    complete DS

---
+++ stack_doubly_linked_list.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+    Node *prev;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+        this->prev = NULL;
+    }
+};
+
+class myStack
+{
+public:
+    Node *head = NULL;
+    Node *tail = NULL;
+    int count = 0;
+
+    void push(int value)
+    {
+
+        count++;
+        Node *newNode = new Node(value);
+        if (head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        tail->next = newNode;
+        newNode->prev = tail;
+        tail = newNode;
+    }
+
+    void pop()
+    {
+        count--;
+        Node *deleteNode = tail;
+        tail = tail->prev;
+        delete deleteNode;
+        if (tail == NULL)
+        {
+            head = NULL;
+            return;
+        }
+        tail->next = NULL;
+    }
+
+    int top()
+    {
+        return tail->value;
+    }
+
+    int size()
+    {
+        return count;
+    }
+
+    bool empty()
+    {
+        return head == NULL;
+    }
+};
+int main()
+{
+
+    myStack st;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++)
+    {
+        int x;
+        cin >> x;
+        st.push(x);
+    }
+
+    while (!st.empty())
+    {
+        cout << st.top() << endl;
+        st.pop();
+    }
+
+    return 0;
+}
